31a3f33b26341d38c843634225e06f156de5b57d
tree <tree3170@gmail.com>
2020-02-10 14:13:23 +0800

    肺炎示例

---
+++ src/main/java/com/tree/ncov/NcovDemoApplication.java
@@ -0,0 +1,16 @@
+package com.tree.ncov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tree
+ */
+@SpringBootApplication
+public class NcovDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcovDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tree/ncov/ncovdemo/NcovDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tree.ncov.ncovdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcovDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
